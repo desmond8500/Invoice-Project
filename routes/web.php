c6c058dc79de90c4041d3a616198cfdde7eb9f37
Kratos <desmond8500@gmail.com>
2020-04-29 20:04:06 +0000

    save

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,5 @@ Route::post('generator_builder/rollback', '\InfyOm\GeneratorBuilder\Controllers\
 Route::post('generator_builder/generate-from-file','\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@generateFromFile')->name('io_generator_builder_generate_from_file');
 
 
+
+
